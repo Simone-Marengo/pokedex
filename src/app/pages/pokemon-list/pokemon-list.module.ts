aeb9993081f71f0db675a2b8eff548985590aed2
Dario De Marco <dario.demarco1208@gmail.com>
2020-12-26 14:27:59 +0100

    Added type labels in pokemon list

--- src/app/pages/pokemon-list/pokemon-list.module.ts
+++ src/app/pages/pokemon-list/pokemon-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { IonicModule } from "@ionic/angular";
 import { TranslateModule } from '@ngx-translate/core';
+import { TypeLabelModule } from "src/app/shared/components/type-label/type-label.module";
 import { SharedModule } from 'src/app/shared/services/shared-module.module';
 import { PokemonListPageRoutingModule } from "./pokemon-list-routing.module";
 import { PokemonListPage } from "./pokemon-list.page";
@@ -11,6 +12,7 @@ import { PokemonListPage } from "./pokemon-list.page";
     PokemonListPageRoutingModule,
     TranslateModule.forChild(),
     SharedModule,
+    TypeLabelModule
   ],
   declarations: [PokemonListPage],
 })
